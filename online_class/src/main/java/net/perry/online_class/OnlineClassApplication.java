897fcb1048d3351912df46d7f89023849548d672
perry <peilin.yuan@aipocloud.com>
2023-10-09 17:16:32 +0800

    项目框架搭建，添加依赖

---
+++ online_class/src/main/java/net/perry/online_class/OnlineClassApplication.java
@@ -0,0 +1,13 @@
+package net.perry.online_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClassApplication.class, args);
+	}
+
+}
---
+++ online_class/src/test/java/net/perry/online_class/OnlineClassApplicationTests.java
@@ -0,0 +1,13 @@
+package net.perry.online_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
