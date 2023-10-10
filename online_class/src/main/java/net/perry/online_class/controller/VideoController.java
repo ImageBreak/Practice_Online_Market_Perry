b8ed8268e18a6a8bb7748faf3f56e41d6bd47509
perry <peilin.yuan@aipocloud.com>
2023-10-10 16:55:45 +0800

    JsonData返回数据封装实现

--- online_class/src/main/java/net/perry/online_class/controller/VideoController.java
+++ online_class/src/main/java/net/perry/online_class/controller/VideoController.java
@@ -1,25 +1,27 @@
 package net.perry.online_class.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.perry.online_class.service.VideoService;
+import net.perry.online_class.domain.Video;
+import net.perry.online_class.service.impl.VideoServiceImpl;
+import net.perry.online_class.utils.JsonData;
 
 @RestController
-@RequestMapping("video")
+@RequestMapping("api/v1/pub/video")
 public class VideoController {
 
     @Autowired
-    private VideoService videoService;
+    private VideoServiceImpl videoServiceImpl;
 
     @RequestMapping("list")
-    public Object listVideo(){
+    public JsonData listVideo(){
         
-        return videoService.listVideo();
+        List<Video> videoList = videoServiceImpl.listVideo();
+        return JsonData.buildSuccess(videoList);
     } 
     
 }
