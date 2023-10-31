f165bffa7cf04d8ab027ada44f894317457915e0
perry <peilin.yuan@aipocloud.com>
2023-10-31 16:16:34 +0800

    后端请求API模块

---
+++ online_class_front/src/request.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const service = axios.create({
+    //url = base url + request url
+    baseURL : "http://127.0.0.1:8081",
+
+    //配置请求超时时间
+    timeout : 5000
+})
+
+export default service
